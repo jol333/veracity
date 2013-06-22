88f9dd15a7fc1f48788fa2265815d096b0f00fb0
Joyal <joyalks@live.com>
2013-06-23 05:15:36 +0530

    Completed a basic version of Facebook RSS feed finder.

--- index.php
+++ index.php
@@ -12,10 +12,55 @@
 	</form>
 
 <?php
+$flag=0;
 if (!empty($_POST))
 {
-	echo "I got it:";
-	echo $_REQUEST['fburl'];
+	$newurl=str_replace("http://", "", $_REQUEST['fburl']);
+	$newurl=str_replace("https://", "", $newurl);
+	
+	if(filter_var("http://".$newurl,FILTER_VALIDATE_URL))
+	{
+		if(strstr($newurl,"fb.com/")||strstr($newurl, "facebook.com/")||strstr($newurl, "fb.me/"))
+			$flag=1;
+		else
+		{
+			$flag=0;
+			echo '<FONT FACE="Monospace" COLOR="red"><B>';
+			echo "Invalid URL!";
+			echo "</B></FONT>";
+		}
+	}
+	else
+	{
+			$flag=0;
+			echo '<FONT FACE="Monospace" COLOR="red"><B>';
+			echo "Invalid URL!";
+			echo "</B></FONT>";
+	}
+
+	if($flag==1)
+	{
+		$urlslice=explode("/", $newurl);
+		$pagename=explode("?",$urlslice[1]);
+		$data=@file_get_contents("http://graph.facebook.com/".$pagename[0]);
+		
+		$pageid=array();
+		if(preg_match('/^.*"id":"([0-9]+)"/', $data,$pageid))
+		{
+			$atom="https://www.facebook.com/feeds/page.php?format=atom10&id=".$pageid[1];
+			$rss20="http://www.facebook.com/feeds/page.php?format=rss20&id=".$pageid[1];
+			echo '<div class="rssfeed">';
+			echo "<a href=".$atom.">".$atom."</a><br>";
+			echo "<a href=".$rss20.">".$rss20."</a>";
+			echo '</div>';
+		}
+		else
+		{
+			echo '<FONT FACE="Monospace" COLOR="red"><B>';
+			echo "RSS feed not found!";
+			echo "</B></FONT>";
+		}
+	}
 }
 ?>
 
